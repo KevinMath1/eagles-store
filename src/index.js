a6f64d87f7ab70ef672ff09f0ede5714dc890012
Kevin <Kevin@gmail.com>
2024-05-16 11:07:36 -0300

    dei um enter

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/home";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+
 root.render(
   <React.StrictMode>
     <App />
