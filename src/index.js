bdad2e3425df6135f7d67525b241c6a9ae7aba52
Gianluca <Gianluca@gmail.com>
2024-05-16 11:03:57 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/home";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
